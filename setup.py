5ff4e35c80d65caf3a9449d55cddea7a2a0245f1
Andreas Ödling <aodling33@gmail.com>
2020-11-20 12:56:47 +0100

    Added some shell of RS integration scripts, a setup script for pip and a pkg declaration for rs integration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rfTool2',
+    version='0.0.1',
+    packages=['rf_helpers', 'common_math', 'example_setups', 'rs_integration'],
+    url='https://github.com/aodling/rfTool2',
+    license='BSD 3-Clause',
+    author='Andreas Ödling',
+    author_email='aodling33@gmail.com',
+    description='A tool for playing around with RF Stuff'
+)
