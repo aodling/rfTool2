b73a8c4239da472a9bdb434c409f5cc337e7b2e7
Andreas Ödling <aodling33@gmail.com>
2021-01-29 09:35:11 +0100

    Removed tons of PEP warnings...

--- main.py
+++ main.py
@@ -4,9 +4,19 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import shutil
 import subprocess
-from  example_setups import doble_tone
+from example_setups import doble_tone
 from time import time
-do_plots =  True
+from example_setups import single_tone_config
+from example_setups import single_tone_config_10G
+from example_setups.rfsignal import rfsignal
+# from example_setups.temp_config import temp_config
+# from example_setups.temp_small_config import temp_small_config
+from rf_helpers.rf_controller import rfController
+from rf_helpers import configuration_generator
+from common_math.math import safe_log10
+from pathlib import Path
+
+do_plots = True
 try:
     import matplotlib.pyplot as plt
     import numpy as np
@@ -14,21 +24,14 @@ except ModuleNotFoundError:
     do_plots = False
     print("Ignoring plots")
 
-from example_setups import single_tone_config
-from example_setups import single_tone_config_10G
-from example_setups.rfsignal import rfsignal
-from example_setups.temp_config import temp_config
-from example_setups.temp_small_config import temp_small_config
-from rf_helpers.rf_controller import rfController
-from rf_helpers import configuration_generator
-from common_math.math import safe_log10
-from pathlib import Path
 use_spec = True
 try:
-    from rs_integration.rs_integration import instrument_init, clear_specan, do_basic_sweep, disconnect, store_config_string
+    from rs_integration.rs_integration import instrument_init, clear_specan, do_basic_sweep, disconnect, \
+        store_config_string
 except ModuleNotFoundError as e:
-    raise e
+
     use_spec = False
+    raise e
 
 
 def print_hi(name):
@@ -36,49 +39,51 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-
 def get_configurations():
     pass
 
 
-def get_adc_string(y):
-    ret =""
-    for v in y:
+def get_adc_string(y_in):
+    ret = ""
+    for v in y_in:
         ret += "{:d}\n".format(v)
     return ret
 
-def download_cfg(d,filename : str):
-    cmd = ["scp","-i", "C:\\MinGW\\msys\\1.0\\home\\TRx\\.ssh\\id_rsa", Path(d) / filename, "root@192.168.0.10:datat.txt"]
+
+def download_cfg(d, filename: str):
+    cmd = ["scp", "-i", "C:\\MinGW\\msys\\1.0\\home\\TRx\\.ssh\\id_rsa", Path(d) / filename,
+           "root@192.168.0.10:datat.txt"]
     print(cmd)
-    #print(" ".join(cmd))
+    # print(" ".join(cmd))
     print("Downloading config {}".format(d))
     r = subprocess.run(cmd, timeout=100, text=True, capture_output=True)
     print("Download Completed. Loading vector...")
-    cmdrun = ["ssh","-i", "C:\\MinGW\\msys\\1.0\\home\\TRx\\.ssh\\id_rsa", "root@192.168.0.10",
+    cmdrun = ["ssh", "-i", "C:\\MinGW\\msys\\1.0\\home\\TRx\\.ssh\\id_rsa", "root@192.168.0.10",
               "./ad9083_xtra/app_ads9/debug/ad9081_xtra tx --file datat.txt"]
-    subprocess.run(cmdrun,timeout = 1000, text=True, capture_output=True)
+    r = subprocess.run(cmdrun, timeout=1000, text=True, capture_output=True)
     print("Downloaded vector.")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    specan = None
     p = rfController(12e9)
 
-    y = p.get_samples([rfsignal(5e9,-12)])
+    y = p.get_samples([rfsignal(5e9, -12)])
     x = p.get_xaxis()
     if do_plots:
         fig, axs = plt.subplots(3, 1, constrained_layout=True)
 
-        axs[0].plot(x,y)
+        axs[0].plot(x, y)
         axs[0].set_title("Time plot")
         axs[0].set_xlabel("Time (s)")
         axs[0].set_ylabel("ADC Value")
 
-        Y = np.fft.fft(y/p.get_max_mag())
-        #Windowing function
-        win = np.ones( len(Y),dtype = int)
-        #Magnitude spectrum with compensation for real spectrum  and windowing
+        Y = np.fft.fft(y / p.get_max_mag())
+        # Windowing function
+        win = np.ones(len(Y), dtype=int)
+        # Magnitude spectrum with compensation for real spectrum  and windowing
         s_mag = np.abs(Y) * 2 / np.sum(win)
         s_dbfs = 20 * safe_log10(s_mag)
         freq = np.fft.fftfreq(len(Y), d=p.Ts)
@@ -87,10 +92,10 @@ if __name__ == '__main__':
         axs[1].set_title("Spectral Plot")
         axs[1].set_xlabel("Frequency (Hz)")
         axs[1].set_ylabel("Power (dBFS)")
-        axs[1].axis([np.min(freq), np.max(freq), -100,0])
+        axs[1].axis([np.min(freq), np.max(freq), -100, 0])
         axs[1].grid(True)
 
-        Y = np.fft.fft(np.concatenate((y,y)) / p.get_max_mag())
+        Y = np.fft.fft(np.concatenate((y, y)) / p.get_max_mag())
         # Windowing function
         win = np.ones(len(Y), dtype=int)
         # Magnitude spectrum with compensation for real spectrum  and windowing
@@ -105,15 +110,14 @@ if __name__ == '__main__':
         axs[2].axis([np.min(freq), np.max(freq), -100, 0])
         axs[2].grid(True)
 
+        # plt.show()
 
-        #plt.show()
-
-    #print(get_adc_string(y))
+    # print(get_adc_string(y))
     # This should be like this
-    #stc = temp_config()
+    # stc = temp_config()
     # But now it is
     startTime = time()
-    dc  = single_tone_config.default_config()
+    dc = single_tone_config.default_config()
     stc = single_tone_config.single_tone_config()
     ttc = doble_tone.two_tone_config()
     tenc = single_tone_config_10G.single_tone_config_10G()
@@ -122,15 +126,15 @@ if __name__ == '__main__':
     configuration_generator.generate_ad_file(stc.config_list[0], path, p, filename)
     i = 0
     confs_to_run = [dc]
-    confs_to_run = [dc, tenc, stc, ttc]
-    confs_to_run = [dc, ttc]
+    # confs_to_run = [dc, tenc, stc, ttc]
+    # confs_to_run = [dc, ttc]
     for c in confs_to_run:
         c.get_cfg_length()
     if use_spec:
         specan = instrument_init("10.10.0.231")
         print("Clearing data on SPECAN")
-        clear_specan(specan,True)
-        #Clear all previous data
+        clear_specan(specan, True)
+        # Clear all previous data
     for config in confs_to_run:
         i = 0
         try:
@@ -138,27 +142,28 @@ if __name__ == '__main__':
         except FileNotFoundError:
             pass
         for cfg in config:
-            path = "{}/{}".format(config.get_path(),config.get_filename(i))
+            path = "{}/{}".format(config.get_path(), "gen_data")  # config.get_filename(i))
             filename = config.get_filename(i)
             configuration_generator.generate_ad_file(cfg, path, p, filename)
             i += 1
             # Configure Spec
             if use_spec:
-                #DOn't download if no spec is configured
+                # DOn't download if no spec is configured
                 download_cfg(path, filename + ".txt")
                 if cfg.get_max_power() > -20:
                     reflev = 0
                 else:
                     reflev = -20
-                do_basic_sweep(specan,output_folder=config.get_path(),span_MHz=config.get_span_MHz(),filename = filename,
-                               rbw_khz=config.get_rbw_kHz(),rlev=reflev)
-                store_config_string(specan,output_folder=config.get_path(),filename=filename)
+                do_basic_sweep(specan, output_folder=config.get_path(), span_MHz=config.get_span_MHz(),
+                               filename=filename,
+                               rbw_khz=config.get_rbw_kHz(), rlev=reflev)
+                store_config_string(specan, output_folder=config.get_path(), filename=filename)
     if use_spec:
         disconnect(specan)
     stopTime = time()
     print(f'Total Runtime: {stopTime - startTime:.3f} secs')
 
-            #Get Data From Spec to Path
+    # Get Data From Spec to Path
 """    i = 0
     path = ttc.get_path()
     try:
@@ -171,6 +176,3 @@ if __name__ == '__main__':
         configuration_generator.generate_ad_file(cfg, path, p, filename)
         i += 1
 """
-
-
-
