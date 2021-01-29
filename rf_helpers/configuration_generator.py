1eb108d3dff9680925d970ffc621eeb6e1d84054
Andreas Ödling <aodling33@gmail.com>
2021-01-29 09:46:55 +0100

    Removed tons of PEP warnings...

--- rf_helpers/configuration_generator.py
+++ rf_helpers/configuration_generator.py
@@ -13,18 +13,23 @@ def generate_ad_file(cfg: example_setups.setup, path: str, controller: rf_helper
 
     samples = controller.get_samples(cfg.get_signals())
     print("Creating directory {}".format(path))
-    pathlib.Path(path).mkdir(parents = True, exist_ok = True)
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path, "gen_info").mkdir(parents=True, exist_ok=True)
+    pathlib.Path(path, "plots").mkdir(parents=True, exist_ok=True)
     adfile = filename + ".txt"
     try:
-        with open(pathlib.Path(path,adfile), 'w') as fp:
+        with open(pathlib.Path(path, adfile), 'w') as fp:
             for s in samples:
                 fp.write("{:d}\n".format(s))
-        with open(pathlib.Path(path,"gen_info.txt"),'w') as fp:
-            fp.write("Generated Stimuli file with frequency: {} MHz @ {} dBFS\n".format(controller.get_real_frequency(freq) / 1e6,amp))
-            fp.write("The requested frequency was {} MHz, a difference of {} Hz\n".format(freq/1e6,controller.get_real_frequency(freq) - freq))
+        with open(pathlib.Path(path, "gen_info", "{}.info".format(filename)), 'w') as fp:
+            fp.write("Generated Stimuli file with frequency: {} MHz @ {} dBFS\n".format(
+                controller.get_real_frequency(freq) / 1e6, amp))
+            fp.write("The requested frequency was {} MHz, a difference of {} Hz\n".format(freq / 1e6,
+                                                                                          controller.get_real_frequency(
+                                                                                              freq) - freq))
     except IOError as e:
         print("Couln't create file")
         raise e
-    plot = pathlib.Path(path,filename + ".png")
-    #Create a spectral plot of the signal as well
+    plot = pathlib.Path(path, "plots", filename + ".png")
+    # Create a spectral plot of the signal as well
     rf_helpers.rf_controller.plot_spectrum(samples, controller, plot)
